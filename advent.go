965cb3600acdb1c2346a8461e8479a1b7eda2df7
formicas <formicas@gmail.com>
2019-12-17 23:34:26 +1030

    day 17 was rather more manual than I would have liked

--- advent.go
+++ advent.go
@@ -3,94 +3,87 @@ package main
 import (
 	"fmt"
 
-	"advent.2019/day1"
-	"advent.2019/day10"
-	"advent.2019/day11"
-	"advent.2019/day12"
-	"advent.2019/day14"
-	"advent.2019/day15"
-	"advent.2019/day16"
-	"advent.2019/day2"
-	"advent.2019/day5"
-	"advent.2019/day6"
-	"advent.2019/day7"
-	"advent.2019/day8"
-	"advent.2019/day9"
+	"advent.2019/day17"
 )
 
 func main() {
-	fmt.Println("---Day 1---")
-	fmt.Println()
-	day1.DoTheThing()
+	// fmt.Println("---Day 1---")
+	// fmt.Println()
+	// day1.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 2---")
+	// fmt.Println()
+	// day2.DoTheThing()
+
+	// //day3 can wait because screw that
+
+	// //day4 can wait because screw that
+
+	// fmt.Println()
+	// fmt.Println("---Day 5---")
+	// fmt.Println()
+	// day5.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 6---")
+	// fmt.Println()
+	// day6.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 7---")
+	// fmt.Println()
+	// day7.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 8---")
+	// fmt.Println()
+	// day8.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 9---")
+	// fmt.Println()
+	// day9.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 10---")
+	// fmt.Println()
+	// day10.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 11---")
+	// fmt.Println()
+	// day11.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 12---")
+	// fmt.Println()
+	// day12.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 13---")
+	// fmt.Println()
+	// //let's not do this every time
+	// //day13.DoTheThing()
+	// fmt.Println("Nah")
+
+	// fmt.Println()
+	// fmt.Println("---Day 14---")
+	// fmt.Println()
+	// day14.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 15---")
+	// fmt.Println()
+	// day15.DoTheThing()
+
+	// fmt.Println()
+	// fmt.Println("---Day 16---")
+	// fmt.Println()
+	// day16.DoTheThing()
 
 	fmt.Println()
-	fmt.Println("---Day 2---")
+	fmt.Println("---Day 17---")
 	fmt.Println()
-	day2.DoTheThing()
-
-	//day3 can wait because screw that
-
-	//day4 can wait because screw that
-
-	fmt.Println()
-	fmt.Println("---Day 5---")
-	fmt.Println()
-	day5.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 6---")
-	fmt.Println()
-	day6.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 7---")
-	fmt.Println()
-	day7.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 8---")
-	fmt.Println()
-	day8.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 9---")
-	fmt.Println()
-	day9.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 10---")
-	fmt.Println()
-	day10.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 11---")
-	fmt.Println()
-	day11.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 12---")
-	fmt.Println()
-	day12.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 13---")
-	fmt.Println()
-	//let's not do this every time
-	//day13.DoTheThing()
-	fmt.Println("Nah")
-
-	fmt.Println()
-	fmt.Println("---Day 14---")
-	fmt.Println()
-	day14.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 15---")
-	fmt.Println()
-	day15.DoTheThing()
-
-	fmt.Println()
-	fmt.Println("---Day 16---")
-	fmt.Println()
-	day16.DoTheThing()
+	day17.DoTheThing()
 }
